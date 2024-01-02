11150f6ac7eff8aea20f30083741999817d77e5b
J56164 <jjclub12099@gmail.com>
2024-01-02 23:11:54 +0700

    Linux support

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
+// #include <conio.h>
+#ifdef __linux__ 
+    //linux code goes here
+    // #include <ncurses.h>
+#elif _WIN32
+    // windows code goes here
+    #include <windows.h>
+    #include <conio.h>
+#endif
 #include <iostream>
 #include <string>
 #include <vector>
-#include <windows.h>
 
 std::vector<std::string> logo = {
     "  ████████ ████████ ████████ ████████  ",
@@ -18,19 +26,56 @@ std::vector<std::string> logo = {
     "  ████████ ████████ ████████ ████████  ",
 };
 
-int main() {
-    SetConsoleOutputCP(CP_UTF8);
-    std::cout << "\e[?1049h";
-    std::cout << "\e[1m";
+const char FULL_BLOCK_CHAR = '\xDB';
+const char TABLE_HORIZONTAL_CHAR = '\xB3';
+const char TABLE_VERTICAL_CHAR = '\xC4';
+const char TABLE_DOWN_LEFT_CHAR = '\xBF';
+const char TABLE_UP_RIGHT_CHAR = '\xC0';
+const char TABLE_UP_LEFT_CHAR = '\xD9';
+const char TABLE_DOWN_RIGHT_CHAR = '\xDA';
+
+void print_logo() {
     for (std::string line: logo) {
-        std::cout << line << std::endl;
+        for (char c: line) {
+            if (c == '#') {
+                std::cout << FULL_BLOCK_CHAR;
+            } else {
+                std::cout << c;
+            }
+        }
+        std::cout << std::endl;
     }
     std::cout << std::endl;
+}
+
+int read_keypress() {
+    int c;
+    #ifdef _WIN32
+        c = getch();
+    #else
+        // getch();
+        std::cin >> c;
+    #endif
+    return c;
+}
+
+// https://en.wikipedia.org/wiki/Code_page_437
+// https://en.wikipedia.org/wiki/ANSI_escape_code
+
+int main() {
+    #ifdef _WIN32
+        SetConsoleOutputCP(65001);
+    #elif __linux__
+        // initscr();
+        // timeout(-1);
+    #endif
+    std::cout << "\e[?1049h"; // Alternate screen on
+    std::cout << "\e[H"; // Move cursor to home
+    std::cout << "\e[?25l"; // Hides cursor
+    print_logo();
     std::cout << "> Play" << std::endl;
     std::cout << "  Quit" << std::endl;
-    std::cout << "\u2788" << std::endl;
-    int i;
-    std::cin >> i;
-    std::cout << "\e[21m";
-    std::cout << "\e[?1049l";
+    read_keypress();
+    std::cout << "\e[?25h"; // Shows cursor
+    std::cout << "\e[?1049l"; // Alternate screen off
 }
