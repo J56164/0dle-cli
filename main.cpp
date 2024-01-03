48646a80bc027252129a9825b3162a43a25fdda3
J56164 <jjclub12099@gmail.com>
2024-01-03 15:05:23 +0700

    commit

--- main.cpp
+++ main.cpp
@@ -1,30 +1,7 @@
-// #include <conio.h>
-#ifdef __linux__ 
-    //linux code goes here
-    // #include <ncurses.h>
-#elif _WIN32
-    // windows code goes here
-    #include <windows.h>
-    #include <conio.h>
-#endif
 #include <iostream>
 #include <string>
 #include <vector>
-
-std::vector<std::string> logo = {
-    "  ████████ ████████ ████████ ████████  ",
-    "                                       ",
-    "█   ████         ██  ████             █",
-    "█ ██    ██       ██    ██             █",
-    "█ █    █ █       ██    ██             █",
-    "█ █   █  █   ██████    ██      ████   █",
-    "█ █  █   █ ██    ██    ██    ██    ██ █",
-    "█ █ █    █ ██    ██    ██    ████████ █",
-    "█ ██    ██ ██    ██    ██    ██       █",
-    "█   ████     ██████ ████████   ██████ █",
-    "                                       ",
-    "  ████████ ████████ ████████ ████████  ",
-};
+#include <curses.h>
 
 const char FULL_BLOCK_CHAR = '\xDB';
 const char TABLE_HORIZONTAL_CHAR = '\xB3';
@@ -34,48 +11,49 @@ const char TABLE_UP_RIGHT_CHAR = '\xC0';
 const char TABLE_UP_LEFT_CHAR = '\xD9';
 const char TABLE_DOWN_RIGHT_CHAR = '\xDA';
 
+std::vector<std::string> logo = {
+    "  ######## ######## ######## ########  \n",
+    "                                       \n",
+    "#   ####         ##  ####             #\n",
+    "# ##    ##       ##    ##             #\n",
+    "# #    # #       ##    ##             #\n",
+    "# #   #  #   ######    ##      ####   #\n",
+    "# #  #   # ##    ##    ##    ##    ## #\n",
+    "# # #    # ##    ##    ##    ######## #\n",
+    "# ##    ## ##    ##    ##    ##       #\n",
+    "#   ####     ###### ########   ###### #\n",
+    "                                       \n",
+    "  ######## ######## ######## ########  \n",
+};
+
 void print_logo() {
     for (std::string line: logo) {
-        for (char c: line) {
-            if (c == '#') {
-                std::cout << FULL_BLOCK_CHAR;
-            } else {
-                std::cout << c;
-            }
-        }
-        std::cout << std::endl;
+        printw(line.c_str());
     }
-    std::cout << std::endl;
+    refresh();
 }
 
 int read_keypress() {
-    int c;
-    #ifdef _WIN32
-        c = getch();
-    #else
-        // getch();
-        std::cin >> c;
-    #endif
+    int c = getch();
     return c;
+}
+
+void title_screen() {
+    print_logo();
+    printw("\n");
+    printw("> Play\n");
+    printw("  Quit\n");
+    while (true) {
+        int keypress = read_keypress();
+    }
 }
 
 // https://en.wikipedia.org/wiki/Code_page_437
 // https://en.wikipedia.org/wiki/ANSI_escape_code
 
 int main() {
-    #ifdef _WIN32
-        SetConsoleOutputCP(65001);
-    #elif __linux__
-        // initscr();
-        // timeout(-1);
-    #endif
-    std::cout << "\e[?1049h"; // Alternate screen on
-    std::cout << "\e[H"; // Move cursor to home
-    std::cout << "\e[?25l"; // Hides cursor
-    print_logo();
-    std::cout << "> Play" << std::endl;
-    std::cout << "  Quit" << std::endl;
-    read_keypress();
-    std::cout << "\e[?25h"; // Shows cursor
-    std::cout << "\e[?1049l"; // Alternate screen off
+    WINDOW* MAIN_WIN = initscr();
+    noecho();
+    title_screen();
+    endwin();
 }
